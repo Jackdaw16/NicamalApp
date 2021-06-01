c2ef77a66cf9918540ae6b3bf3a080d4ffbfa919
Jackdaw16 <alejandro.demetrio13@gmail.com>
2021-06-01 01:06:48 +0100

    Update CustomSearchBarComponent.dart

--- lib/components/CustomSearchBarComponent.dart
+++ lib/components/CustomSearchBarComponent.dart
@@ -16,59 +16,64 @@ class _CustomSearchBarComponentState extends State<CustomSearchBarComponent> {
   final Color greenAccent = Color.fromARGB(255, 24, 157, 139);
   String textFilter;
 
+  bool safeArea (double padding) {
+    if (padding > 0) {
+      return true;
+    }
+
+    return false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        /*Notch size*/
-        SizedBox(
-          height: 60,
+    var padding = MediaQuery.of(context).viewPadding.top;
+    return SafeArea(
+      top: safeArea(padding),
+      child: Padding(
+        padding: EdgeInsets.only(
+          top: 8,
+          bottom: 8,
+          left: 16,
+          right: 16,
         ),
-        Padding(
-          padding: EdgeInsets.only(
-            bottom: 8,
-            left: 16,
-            right: 16,
+        child: Container(
+          width: double.infinity,
+          height: 40,
+          decoration: BoxDecoration(boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.16),
+              spreadRadius: 5,
+              blurRadius: 6,
+              offset: Offset(0, 3),
+            )
+          ]),
+          child: TextField(
+            onChanged: (text) => {
+              setState(() {
+                textFilter = text;
+                widget.filterChange(textFilter);
+              })
+            },
+            textInputAction: TextInputAction.send,
+            decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide.none),
+                filled: true,
+                fillColor: Colors.white,
+                prefixIcon: Icon(Icons.search, color: greenAccent),
+                hintText: 'Search animals',
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.filter_list),
+                  onPressed: () => {print('pressed')},
+                  color: greenAccent,
+                ),
+                contentPadding: EdgeInsets.only(
+                  bottom: 40 / 2, // HERE THE IMPORTANT PART
+                )),
           ),
-          child: Container(
-            width: double.infinity,
-            height: 40,
-            decoration: BoxDecoration(boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.16),
-                spreadRadius: 5,
-                blurRadius: 6,
-                offset: Offset(0, 3),
-              )
-            ]),
-            child: TextField(
-              onChanged: (text) => {
-                setState(() {
-                  textFilter = text;
-                  widget.filterChange(textFilter);
-                })
-              },
-              textInputAction: TextInputAction.send,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
-                      borderSide: BorderSide.none),
-                  filled: true,
-                  fillColor: Colors.white,
-                  prefixIcon: Icon(Icons.search, color: greenAccent),
-                  hintText: 'Search animals',
-                  suffixIcon: IconButton(
-                    icon: Icon(Icons.filter_list),
-                    onPressed: () => {print('pressed')},
-                    color: greenAccent,
-                  ),
-                  contentPadding: EdgeInsets.only(
-                    bottom: 40 / 2, // HERE THE IMPORTANT PART
-                  )),
-            ),
-          ),
-        )
-      ],
+        ),
+      ),
     );
   }
 }
